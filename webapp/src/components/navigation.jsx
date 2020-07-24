a0e3386610e5c933407de082ba4215876646e5ad
yanrchen <uczlheq@ucl.ac.uk>
2020-07-24 14:16:17 +0100

    partialy fix the issue of obtaining the lauguage lists

--- webapp/src/components/navigation.jsx
+++ webapp/src/components/navigation.jsx
@@ -6,56 +6,77 @@ export default class Navigation extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            voice: null,
+            voices: null,
         };
 
         //binding
-        this.speakTexts = this.speakTexts.bind(this);
+        //this.speakTexts = this.speakTexts.bind(this);
         this.obtainVoices = this.obtainVoices.bind(this);
     }
 
-    obtainVoices() {
 
-    }
 
     // receive info using websocket to start navigation
-
-    speakTexts() {
-        var voices = window.speechSynthesis.getVoices();//get language lists
-
-        //setInterval(, 10);
-
-
-        var utterThis = new SpeechSynthesisUtterance('hello, we are sight ++!'); // text content
-/*        utterThis.onend = function (event) {
-            console.log('SpeechSynthesisUtterance.onend');
-        }
-        utterThis.onerror = function (event) {
-            console.error('SpeechSynthesisUtterance.onerror');
-        }*/
-
-
-        utterThis.voice = voices[2]; // choose the language type(en-GB)
-        console.log(utterThis.voice);
-
-        utterThis.pitch = 1;// pitch
-        utterThis.rate = 1;// rate
-
-        speechSynthesis.speak(utterThis); //speak
+    async obtainVoices() {
+        console.log("obtainVoices");
+        //const {voices} = this.state;
+        this.state.voices = window.speechSynthesis.getVoices();
+        console.log(this.state.voices);
+        console.log(Object.keys(this.state.voices).length);
     }
 
-    componentDidMount() {
-        // choose the language
 
+
+//     speakTexts() {
+//         // var ifObtained = setInterval(()=> {
+//         //     console.log(voices);
+//         //     console.log(Object.keys(voices).length);
+//         //
+//         //     if (Object.keys(voices).length != 0) {
+//         //         clearInterval(ifObtained);
+//         //     }
+//         // }, 2000);
+//         console.log("speakTexts");
+//         var utterThis = new SpeechSynthesisUtterance('hello, we are sight ++!'); // text content
+// /*        utterThis.onend = function (event) {
+//             console.log('SpeechSynthesisUtterance.onend');
+//         }
+//         utterThis.onerror = function (event) {
+//             console.error('SpeechSynthesisUtterance.onerror');
+//         }*/
+//
+//
+//         utterThis.voice = this.state.voices[2]; // choose the language type(en-GB)
+//
+//         utterThis.pitch = 1;// pitch
+//         utterThis.rate = 1;// rate
+//
+//         speechSynthesis.speak(utterThis); //speak
+//     }
+
+
+
+    componentDidMount() {
+        console.log("component Did Mount   begine");
+        // obtain the language lists
+        this.obtainVoices();
+        if (speechSynthesis.onvoiceschanged !== undefined) {
+            speechSynthesis.onvoiceschanged = this.obtainVoices;
+            console.log(this.state.voices);
+        }
+        console.log(this.state.voices);
+        console.log("component Did Mount   end");
 
         //speak
-        this.speakTexts();
+        //this.speakTexts();
     }
 
 
 
 
     render() {
+        console.log(this.state.voices);
+
         return (
             <div>
                 <h1>We are navigating for you.</h1>
